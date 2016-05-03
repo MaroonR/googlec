cbe266b44687cfa1e43d756416ffa08a6967ea13
arminHadzic <arminhadzic@outlook.com>
2016-05-03 15:49:19 -0400

    Fixed binsearch Bug
    Added condition when abs(neg val) == abs(val) in binsearch.

--- spysnippets.py
+++ spysnippets.py
@@ -17,8 +17,14 @@ def answer(document, searchTerms):
 		# print("list a: %s \t list b: %s" % (lsta, lstb))
 		if((abs(x - lsta[len(lsta)-1])) < (abs(x-lstb[0]))):
 			return binsearch(x, lsta)
+		elif((abs(x - lsta[len(lsta)-1])) == (abs(x-lstb[0]))):
+			if(x - lsta[len(lsta)-1] > 0):
+				return binsearch(x, lsta)
+			else:
+				return binsearch(x, lstb)
 		else:
 			return binsearch(x, lstb)
+
 			
 	#parse document, get tokens
 	tokens = document.split()
@@ -30,7 +36,7 @@ def answer(document, searchTerms):
 		if (i in buckets):
 			buckets[i].append(count)
 		count+=1
-	#print(buckets)
+	#print("THE BUCKETS: ", buckets)
     
     # Now we have bunch of buckets with all the terms' locations.
     # Let's find a set for a single bucket that represents the 
@@ -42,10 +48,13 @@ def answer(document, searchTerms):
 	curr_distances = {k: 501 for k in searchTerms}
 	#print("************************************")
 	count = 0
+	#print("buckets length ", len(buckets))
+	#print("testkey: ", testkey)
+	#print("buckets[testkey] length: ", len(buckets[testkey]))
 	for val in buckets[testkey]:  # for each value in the test bucket
 		curr_distances[testkey] = val
 		for key in buckets:       #we want to search the other buckets
-			#print("Searching key %s" % key)
+			#print("Searching key %s %s %s" % (key, val, buckets[key]))
 			if(key == testkey):
 				#print("skipping...")
 				continue 				#except the test bucket
@@ -71,9 +80,12 @@ def answer(document, searchTerms):
 	smallest_tuple = []
 	for i in distance_records:
 		temp = abs(i[0] - i[len(i)-1]) #needs absolute value!!
+		#print("temp: ", temp, " smallest: ", smallest)
 		if( temp < smallest):
+			#print("OOOO SMALLEST temp: ", temp, " smallest: ", smallest)
 			smallest = temp
 			smallest_tuple = i
+			
 	#print("**SMALLEST TUPLE: %s ***" % smallest_tuple)
 	#we have the smallest distance, and its tuple, now we need to return the sub-list between the min and max
 	return tokens[smallest_tuple[0]: smallest_tuple[len(smallest_tuple)-1]+1] # done!
@@ -83,3 +95,7 @@ print(answer("a b c d a", ['a','c','d']))
 print(answer("many google employees can program", ["google", "program"]))
 print(answer("world there hello hello where world", ["hello", "world"]))
 print(answer(gen_doc(), ['a','c','d']))
+print(answer("231 16 12 15 18 19 17 18 16 15 118", ["231", "15", "118"]))
+print("FAIL CASE ___________________________________________________________________________________")
+print(answer("d c g a s c", ["a", "c", "d"]))
+print("FAIL CASE ___________________________________________________________________________________")
